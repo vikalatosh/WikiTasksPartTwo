075de95e676d981bb027961ae04fd3cd07e2cf82
Viktoryia.Latosh <latosh.viktorija@mail.ru>
2021-08-17 17:14:35 +0300

    Added base class

---
+++ Task3/Animal.cs
@@ -0,0 +1,19 @@
+﻿namespace Task3
+{
+    public abstract class Animal
+    {
+        public int weight;
+        public int amountOfFood;
+        public string typeOfFood;
+
+        public string Name { get; set; }
+
+        public Animal(string name, int weight)
+        {
+            Name = name;
+            this.weight = weight;
+        }
+
+        public abstract int food();
+    }
+}
