98e16f82f9a39407bc35ed24a7a2b6fdb881e2f5
Viktoryia.Latosh <latosh.viktorija@mail.ru>
2021-08-23 15:26:48 +0300

    Fixed

--- Task3/Herbivore.cs
+++ Task3/Herbivore.cs
@@ -17,21 +17,5 @@ namespace Task3
             typeOfFood = "растения";
             return amountOfFood;
         }
-        
-        public int AnimalClass()
-        {
-            Console.WriteLine("Если животное хищное - введите 1. Если животное травоядное - введите 2, " +
-                              "если животное всеядное - введите 3");
-            int animalClass = Convert.ToInt32(Console.ReadLine());
-            // Console.WriteLine("Введите имя животного.");
-            // string name = Console.ReadLine();
-            // Console.WriteLine("Введите вес животного.");
-            // int weight = Convert.ToInt32(Console.ReadLine());
-            // if (animalClass == 1)
-            // {
-            //     new Predator() 
-            // }
-            return animalClass;
-        }
     }
 }
